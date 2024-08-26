d92d21c332b1d7fba8d56112166d4a443bbf3481
Farhan Azeemi <54116191+farhanazeemi@users.noreply.github.com>
2024-08-26 23:42:31 +0500

    app.js

--- Password Strength Checker/app.js
+++ Password Strength Checker/app.js
@@ -1,20 +1,37 @@
 const input = document.querySelector("#password");
 const output = document.getElementById("output");
 
-input.addEventListener("input", function() {
-    let pass = input.value;
-
-    if (pass.length < 8) {
-        output.innerText = "Your password is too short";
-        output.style.color = "red";
-    } else if (pass.length >= 8 && pass.length <= 16) {
-        output.innerText = "Your password is strong";
-        output.style.color = "green";
-    } else if (pass.length > 16 && pass.length <= 32) {
-        output.innerText = "Your password is very strong";
-        output.style.color = "green";
+function checkPasswordStrength(password) {
+    if (password.length < 8) {
+        return { message: "Your password is too short", color: "red" };
+    } else if (password.length > 32) {
+        return { message: "Your password is too long", color: "orange" };
     } else {
-        output.innerText = "Your password is too long";
-        output.style.color = "orange";
+        const strength = validatePassword(password);
+        return { message: strength.message, color: strength.color };
     }
+}
+
+function validatePassword(password) {
+    if (password.search(/[a-z]/) === -1) {
+        return { message: "Password is missing a lowercase letter", color: "red" };
+    } else if (password.search(/[A-Z]/) === -1) {
+        return { message: "Password is missing an uppercase letter", color: "red" };
+    } else if (password.search(/[0-9]/) === -1) {
+        return { message: "Password is missing a number", color: "red" };
+    } else if (password.search(/[@/#/$/%]/) === -1) {
+        return { message: "Password is missing a special character", color: "red" };
+    } else if (password.length >= 8 && password.length <= 16) {
+        return { message: "Your password is strong", color: "green" };
+    } else if (password.length > 16 && password.length <= 32) {
+        return { message: "Your password is very strong", color: "green" };
+    }
+}
+
+input.addEventListener("input", function() {
+    const password = input.value;
+    const strength = checkPasswordStrength(password);
+
+    output.innerText = strength.message;
+    output.style.color = strength.color;
 });
